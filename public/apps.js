a6f2bbd6c20c48ad72cd4ece58575681838e77ca
PepperMarioYT <142949229+PepperMarioYT@users.noreply.github.com>
2024-10-18 18:21:46 -0400

    Update apps.js

--- public/apps.js
+++ public/apps.js
@@ -6,20 +6,6 @@ var apps = [
     image: 'https://games-and-stuff.vercel.app/assets/icons/canvas.jpg',
     description: 'This is the offical Games And Stuff drawing canvas. You can draw whatever you like, all in real-time so people can see what you\'re making. There are some rules though, which are, no drawing bad stuff and keep everything family freindly. Otherwize, have fun!'
   },
-/*  {
-    id: 'youtube',
-    title: 'Youtube',
-    url: 'https://youtube.com',
-    image: 'https://static.vecteezy.com/system/resources/thumbnails/023/986/704/small_2x/youtube-logo-youtube-logo-transparent-youtube-icon-transparent-free-free-png.png',
-    description: 'YouTube is a video-sharing platform where users can watch, upload, and share videos. It is the second most visited website in the world, with over 2.5 billion monthly active users.'
-  },
-  {
-    id: 'tiktok',
-    title: 'TikTok',
-    url: 'https://tiktok.com',
-    image: 'https://sf-tb-sg.ibytedtos.com/obj/eden-sg/uhtyvueh7nulogpoguhm/tiktok-icon2.png',
-    description: 'TikTok is a short-form video-sharing app that allows users to create and share 15-second to 10-minute videos. It is known for its viral dance challenges, lip-syncing videos, and comedy skits.'
-  }, */
   {
     id: 'discord',
     title: 'Discord',
@@ -33,12 +19,5 @@ var apps = [
     url: 'https://google.com',
     image: 'https://img.freepik.com/free-icon/search_318-265146.jpg',
     description: 'Google is an American multinational technology company that specializes in Internet-related services and products, which include online advertising technologies, a search engine, cloud computing, software, and hardware.'
-  },
-/*  {
-    id: 'netflix',
-    title: 'Netflix',
-    url: 'https://netflix.com',
-    image: 'https://deadline.com/wp-content/uploads/2022/08/Netflix_Symbol_logo.jpg?w=500&h=500&crop=1',
-    description: 'Netflix is a popular American entertainment company that provides streaming services for television shows, movies, and documentaries.'
-  } */
+  }
 ]
