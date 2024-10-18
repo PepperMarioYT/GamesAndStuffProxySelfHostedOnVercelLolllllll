21e456c8c984ef62b2a9f7b296ac6408bbb01e8f
PepperMarioYT <142949229+PepperMarioYT@users.noreply.github.com>
2024-10-18 18:22:20 -0400

    Create removedapps.js

---
+++ public/removedapps.js
@@ -0,0 +1,21 @@
+  {
+    id: 'youtube',
+    title: 'Youtube',
+    url: 'https://youtube.com',
+    image: 'https://static.vecteezy.com/system/resources/thumbnails/023/986/704/small_2x/youtube-logo-youtube-logo-transparent-youtube-icon-transparent-free-free-png.png',
+    description: 'YouTube is a video-sharing platform where users can watch, upload, and share videos. It is the second most visited website in the world, with over 2.5 billion monthly active users.'
+  },
+  {
+    id: 'tiktok',
+    title: 'TikTok',
+    url: 'https://tiktok.com',
+    image: 'https://sf-tb-sg.ibytedtos.com/obj/eden-sg/uhtyvueh7nulogpoguhm/tiktok-icon2.png',
+    description: 'TikTok is a short-form video-sharing app that allows users to create and share 15-second to 10-minute videos. It is known for its viral dance challenges, lip-syncing videos, and comedy skits.'
+  },
+ {
+    id: 'netflix',
+    title: 'Netflix',
+    url: 'https://netflix.com',
+    image: 'https://deadline.com/wp-content/uploads/2022/08/Netflix_Symbol_logo.jpg?w=500&h=500&crop=1',
+    description: 'Netflix is a popular American entertainment company that provides streaming services for television shows, movies, and documentaries.'
+  } 
